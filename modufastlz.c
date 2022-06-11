fbc98a8667486b0197ba4b494cfba2d1f26bdd02
Damiano Mazzella <damianomazzella@gmail.com>
2022-06-11 15:52:17 +0200

    Update modufastlz.c

--- modufastlz.c
+++ modufastlz.c
@@ -127,6 +127,6 @@ const mp_obj_module_t mp_module_ufastlz = {
 };
 
 // Register the module to make it available in Python
-MP_REGISTER_MODULE(MP_QSTR__fastlz, mp_module_ufastlz, MICROPY_PY_UFASTLZ);
+MP_REGISTER_MODULE(MP_QSTR__fastlz, mp_module_ufastlz);
 
 #endif // MICROPY_PY_UFASTLZ
